acd65e18ccc14534d2195bb8438c881622cfb6d8
EGPuchol <egpuchol@gmail.com>
2024-01-30 22:18:18 +0100

    housesv1

--- src/components/Houses/Houses.jsx
+++ src/components/Houses/Houses.jsx
@@ -1,12 +1,28 @@
 import { useContext } from "react"
 import { apiCallContext } from "../../Context/Context"
+import ".//Houses.css"
 
 
 export const Houses = () => {
 
     const{houses}=useContext(apiCallContext)
-    console.log("soy houses",houses)
+    // console.log("soy houses",houses)
   return (
-    <div>Houses</div>
+    <>
+    {/* <div>Houses</div> */}
+    
+    <div className="houses_container">
+      {houses.map((houses,index)=>{
+        return(
+          <div key={houses.id} className="card_container">
+          <img key={index} src={`../../..${houses.image}`} alt="houses"></img>
+          <h3>{houses.name}</h3>
+          </div>
+        )
+      })}
+    </div>
+    </>
   )
 }
+
+// /images/characters/
