25d99ad2d3bb540d07b37f01d7e04e6103455daf
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-01 09:15:06 +0100

    subida chronology

--- src/components/Chronology/Chronology.jsx
+++ src/components/Chronology/Chronology.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+
 const Chronology = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  
+  const[characters, setCharacters]= useState([])
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+
+const baseUrl="http://localhost:3001"
+
+
+
+    const getCharacters = async () => {
+    try {
+        const charactersApi = await fetch(`${baseUrl}/characters`);
+        const characterJson = await charactersApi.json();
+        setCharacters(characterJson);
+
+    } catch (error) {
+      console.log(error);
+    }
+    finally {
+      setLoading(false);
+    }
+    
+  };
+  getCharacters();
 }
+, []);
+console.log(characters.sort((a,b)=> a.value - b.value ));
+return (
+  <div>
+    {loading ? (
+      <p>loading</p>
+    ) : (
+      <h1>holaaa</h1>
+    )}
+  </div>
+);
+};
 
 export default Chronology
