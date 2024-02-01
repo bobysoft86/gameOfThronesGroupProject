98861435a5bd765fdcb8fe03acf4025c9cfe12ad
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-01 15:11:52 +0100

    barra houses

--- src/components/Houses/Houses.jsx
+++ src/components/Houses/Houses.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import ".//Houses.css"
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
+import SimpleBar from "simplebar-react";
 
 
 export const Houses = () => {
@@ -13,8 +14,10 @@ export const Houses = () => {
   return (
     <>
     {/* <div>Houses</div> */}
+    <SimpleBar style={{maxHeight:"70vh"}} minSize={20} autoHide={false} >
     <div className="houses_container">
     <Nav></Nav>
+
       {houses.map((houses,index)=>{
         return(
           <div  key={houses.id} className="card_container">
@@ -25,6 +28,7 @@ export const Houses = () => {
       })}
     <Footer></Footer>
     </div>
+      </SimpleBar>
     </>
   )
 }
