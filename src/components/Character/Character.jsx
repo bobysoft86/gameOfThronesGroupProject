c342fb0d242825e9cf35ae28e3589a30934a74f4
Fernando Giannattasio <fsgiannattasio@gmail.com>
2024-02-02 17:56:21 +0100

    estilosnuevos

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -43,7 +43,7 @@ const imagenCasa= houses.filter((house)=> house.name === character.house);
         <div className="nav_character_container">
           <div className="nav_character_container_left">
             <Link to = {"/characters"}><img className="flecha" src="../../../images/nav_images/flecha.png" alt="spain-flag"></img></Link>
-            <Link to = {"/characters"} className="volver"><p>Volver</p></Link>
+            <Link to = {"/characters"}><p className="volver">Volver</p></Link>
           </div>
           <div className="nav_character_container_right">
             <div className="home">
@@ -71,12 +71,14 @@ const imagenCasa= houses.filter((house)=> house.name === character.house);
               <img src={`../../..${imagenCasa[0].image}`} alt="" />
             </div>
             <div className="data">
+              <div className="scroll">
               <h3>Alianzas</h3>
               {character.alliances.map((alliance, index) => (
                 <p className="palianzas" key={index}>
                   {alliance}
                 </p>
               ))}
+              </div>
             </div>
             <div className="data">
               <h3>Apariciones</h3>
